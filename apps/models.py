a4f5b2cc2e106293a65f4aceee71b476d3f1ab7a
crowdbotics-apps <team@crowdbotics.com>
2023-01-03 16:25:08 +0000

    Update models

--- apps/models.py
+++ apps/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class App(models.Model):
     'Generated Model'
     name = models.TextField()
+    description = models.TextField(null=True,blank=True,)
 
 # Create your models here.
