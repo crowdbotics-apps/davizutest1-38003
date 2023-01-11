1a6f6bf38e84d4404933fb4a1563ac9c01448fd8
crowdbotics-apps <team@crowdbotics.com>
2023-01-11 19:32:23 +0000

    Update models

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -4,8 +4,8 @@ class Plan(models.Model):
     'Generated Model'
     name = models.CharField(max_length=20,)
     description = models.TextField()
-    price = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True,)
     updated_at = models.DateTimeField(auto_now=True,)
+    price = models.FloatField(null=True,blank=True,)
 
 # Create your models here.
