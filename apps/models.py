4bd19e201d89741b5b75f4a54e2acb281e7e13e4
crowdbotics-apps <team@crowdbotics.com>
2023-01-11 18:46:14 +0000

    Update models

--- apps/models.py
+++ apps/models.py
@@ -2,10 +2,15 @@ from django.conf import settings
 from django.db import models
 class App(models.Model):
     'Generated Model'
-    name = models.TextField()
     description = models.TextField(null=True,blank=True,)
     type = models.ForeignKey("apps.Type",null=True,blank=True,on_delete=models.SET_NULL,related_name="app_type",)
-    framework = models.ForeignKey("apps.Framework",on_delete=models.SET_NULL,null=True,blank=True,related_name="app_framework",)
+    framework = models.ForeignKey("apps.Framework",null=True,blank=True,on_delete=models.SET_NULL,related_name="app_framework",)
+    domain_name = models.CharField(max_length=50,null=True,blank=True,)
+    name = models.CharField(max_length=50,null=True,blank=True,)
+    screenshot = models.URLField(null=True,blank=True,)
+    user = models.ForeignKey("users.User",on_delete=models.SET_NULL,null=True,blank=True,related_name="app_user",)
+    create_at = models.DateTimeField(auto_now_add=True,null=True,blank=True,)
+    updated_at = models.DateTimeField(auto_now=True,null=True,blank=True,)
 class Type(models.Model):
     'Generated Model'
     name = models.TextField()
--- apps/views.py
+++ apps/views.py
@@ -1,4 +1,4 @@
-from .models import App, Type
+from .models import Framework, App, Type
 from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
 
