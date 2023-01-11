ce2ecd67b4bc0cca8f495d4cde0889e7a6ff5c59
crowdbotics-apps <team@crowdbotics.com>
2023-01-11 18:32:02 +0000

    Update models

--- apps/models.py
+++ apps/models.py
@@ -4,8 +4,8 @@ class App(models.Model):
     'Generated Model'
     name = models.TextField()
     description = models.TextField(null=True,blank=True,)
-    framework = models.IntegerField(null=True,blank=True,)
     type = models.ForeignKey("apps.Type",null=True,blank=True,on_delete=models.SET_NULL,related_name="app_type",)
+    framework = models.ForeignKey("apps.Framework",on_delete=models.SET_NULL,null=True,blank=True,related_name="app_framework",)
 class Type(models.Model):
     'Generated Model'
     name = models.TextField()
